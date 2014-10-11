7ab0ca4de8f544bc5ef96d7a08ea76aff83b1e40
Denis <unsumulum@gmail.com>
2014-10-12 01:41:02 +0400

    request params

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function () {
-    var dataUrl = 'https://www.google.com/async/newtab';
+    var dataUrl = 'https://www.google.com/async/newtab?async=xid:1,_fmt:json&espv=2&yv=1';
 
     var request = function (url, callback) {
         var statechange = function () {
@@ -20,17 +20,29 @@
 
     request(dataUrl, function () {
         var obj = JSON.parse(this.responseText.slice(4));
-        var strCss = obj.update.og.css.replace(/\/\/ssl.gstatic./g, 'https://ssl.gstatic.');
-        document.getElementById('loadcss').innerHTML = strCss;
+        var css = obj.update.og.css;
+        var html = obj.update.og.up;
+        
+        if (css) {
+            css = css.replace(/\/\/ssl.gstatic./g, 'https://ssl.gstatic.');
+            document.getElementById('loadcss').innerHTML = css;
+        }
         
         var tmp = document.createElement('div');
-        tmp.innerHTML = obj.update.og.html;
+        tmp.innerHTML = html;
         var elements = tmp.getElementsByTagName('li');
-        var div = elements[0].parentNode;
-        for (var i = 0; i < div.childNodes.length; i++) {
-            div.childNodes[i].addEventListener('click', clickHandler);
+        if (elements.length) {
+            var div = elements[0].parentNode;
+            for (var i = 0; i < div.childNodes.length; i++) {
+                div.childNodes[i].addEventListener('click', clickHandler);
+            }
+            document.body.appendChild(div);
+        } else {
+            var msgElement = document.createElement('span');
+            msgElement.style['white-space'] = 'nowrap';
+            msgElement.innerText = chrome.i18n.getMessage('error_message');
+            document.body.appendChild(msgElement);
         }
-        document.body.appendChild(div);
     });
 
 })();
