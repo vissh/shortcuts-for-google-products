fe1a1a18b7c2af08ac3416aa858dd144531155a0
Denis <unsumulum@gmail.com>
2015-03-08 15:34:11 +0300

    Сохранение последнего результата

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function () {
+document.addEventListener('DOMContentLoaded', function () {
     var dataUrl = 'https://www.google.com/async/newtab?async=xid:1,_fmt:json&espv=2&yv=1';
 
     var request = function (url, callback) {
@@ -18,10 +18,19 @@
         window.open(this.childNodes[0].href);
     };
 
+    var savedHtml = localStorage.getItem('savedHtml');
+    var originHtml = document.documentElement.innerHTML;
+
+    if (savedHtml) {
+        document.documentElement.innerHTML = savedHtml;
+    }
+
     request(dataUrl, function () {
         var obj = JSON.parse(this.responseText.slice(4));
         var css = obj.update.og.css;
         var html = obj.update.og.up;
+
+        document.documentElement.innerHTML = originHtml;
         
         if (css) {
             css = css.replace(/\/\/ssl.gstatic./g, 'https://ssl.gstatic.');
@@ -31,12 +40,15 @@
         var tmp = document.createElement('div');
         tmp.innerHTML = html;
         var elements = tmp.getElementsByTagName('li');
+        
         if (elements.length) {
             var div = elements[0].parentNode;
             for (var i = 0; i < div.childNodes.length; i++) {
                 div.childNodes[i].addEventListener('click', clickHandler);
             }
+            document.body.innerHTML = '';
             document.body.appendChild(div);
+            localStorage.setItem('savedHtml', document.documentElement.innerHTML);
         } else {
             var msgElement = document.createElement('span');
             msgElement.style['white-space'] = 'nowrap';
@@ -45,4 +57,4 @@
         }
     });
 
-})();
+});
